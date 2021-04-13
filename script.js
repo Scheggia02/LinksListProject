f8cd93f24b5185c3cdb04e5d3a38536e2e135c92
Dom <dom0216sal@outlook.it>
2021-04-13 12:43:34 +0200

    Added JQuery implementation

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+const sitesList = [];
+
 const init = function() {
     $("form").submit(function(event) 
     {
         event.preventDefault();
         var urlText = $("#url-input").val();
         var titleText = $("#title-input").val();
+        var categoryText = $("#category-input").val();
         if(urlText == null || titleText == null) return;
+
+        const siteObject = {
+            url: urlText,
+            title: titleText,
+            category: categoryText
+        }
+
+        sitesList.push(siteObject);
 
         const listItem = $("<a>").attr("href",  urlText).text(titleText);
         const itemContent = $("<li>").append(listItem);
